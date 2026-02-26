8ad1cd45ee8433e606e37714c01839f450d3ee84
Fabio Alfieri <54310352+fabio-alfieri@users.noreply.github.com>
2026-02-26 15:00:19 -0500

    Update plots.R
    update AssayHeatmap

--- R/plots.R
+++ R/plots.R
@@ -298,6 +298,7 @@ assayHeatmap <- function(TapestriExperiment, alt.exp = NULL, assay = NULL,
   } else {
     
     row.order <- hm.matrix[which(rownames(hm.matrix) %in% chromosome.cluster),]
+    row.order <- t(as.data.frame(apply(row.order, 2, mean)))
     row.order <- row.order[,hclust(dist(t(row.order)))$order]
     row.order <- colnames(row.order)
     
