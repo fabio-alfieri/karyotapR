5c8508856ec1b1e47f12eb4ec3d9b632b8f8c366
Fabio Alfieri <54310352+fabio-alfieri@users.noreply.github.com>
2026-07-03 15:48:10 -0400

    Update copyNumberCalculation.R

--- R/copyNumberCalculation.R
+++ R/copyNumberCalculation.R
@@ -326,9 +326,18 @@ calcSmoothCopyNumber <- function(TapestriExperiment, method = "weighted.median",
       if(linear.correction){
         cli::cli_progress_step("Applying linear correction on SmoothedCopyNumberByChr.", )
         
-        reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, cluster) %>% 
-          filter(cluster == control.copy.number$sample.label[1]) %>%
-          pull(cell.barcode)
+        tryCatch({
+          reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, cluster) %>% 
+            filter(cluster == control.copy.number$sample.label[1]) %>%
+            pull(cell.barcode)
+          if(is_empty(reference.barcodes)){
+            tryCatch({
+              reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, sample.id) %>% 
+                filter(sample.id == control.copy.number$sample.label[1]) %>%
+                pull(cell.barcode)
+            })
+          }
+        })
         
         control.copy.number_chr <- control.copy.number
         control.copy.number_chr$feature.id <- sub("^chr([0-9]+|X|Y).*", "\\1", control.copy.number_chr$arm)
@@ -379,9 +388,18 @@ calcSmoothCopyNumber <- function(TapestriExperiment, method = "weighted.median",
       if(linear.correction){
         cli::cli_progress_step("Applying linear correction on SmoothedCopyNumberByArm.", )
         
-        reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, cluster) %>% 
-          filter(cluster == control.copy.number$sample.label[1]) %>%
-          pull(cell.barcode)
+        tryCatch({
+          reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, cluster) %>% 
+            filter(cluster == control.copy.number$sample.label[1]) %>%
+            pull(cell.barcode)
+          if(is_empty(reference.barcodes)){
+            tryCatch({
+              reference.barcodes <- getTidyData(TapestriExperiment) %>% select(cell.barcode, sample.id) %>% 
+                filter(sample.id == control.copy.number$sample.label[1]) %>%
+                pull(cell.barcode)
+            })
+          }
+        })
         
         control.copy.number_arm <- control.copy.number
         control.copy.number_arm$feature.id <- control.copy.number_arm$arm
